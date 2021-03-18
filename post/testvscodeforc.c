4190c95659559f927bc8a0958064275f163c833c
Sky <156252108@qq.com>
2021-03-18 18:09:22 +0800

    sky

---
+++ post/testvscodeforc.c
@@ -0,0 +1,12 @@
+/**
+ * @Author: ZhengKai
+ * @Date:   2021-03-18 14:12:30
+ * @Last Modified by:   ZhengKai
+ * @Last Modified time: 2021-03-18 14:15:14
+ */
+#include <stdio.h>
+int main(void)
+{
+    printf("HelloWorld!\n");
+    return 0;
+}
